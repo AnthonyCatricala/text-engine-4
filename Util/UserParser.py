ee455c03d536d26f5cf7e868c1aa69010760bd75
oliveriolucy@yahoo.com <oliveriolucy@yahoo.com>
2018-10-16 14:12:02 -0400

    Still testing github

--- Util/UserParser.py
+++ Util/UserParser.py
@@ -1,2 +1,11 @@
 class UserParser:
-    print("hi")
+    from Util.ItemUtil import *
+    import json
+    import os
+    room = None
+    def __init__(self, room_name):
+        self.room = self.os.path.realpath(room_name, ".json")
+
+    room = os.path.realpath("Example")
+
+
