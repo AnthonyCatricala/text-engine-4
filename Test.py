6036c838819444ce66ef1b622478b237560b33a4
oliveriolucy@yahoo.com <oliveriolucy@yahoo.com>
2018-10-25 15:00:10 -0400

    Merge branch 'master' of https://github.com/AnthonyCatricala/text-engine-4 into command-parser

--- Test.py
+++ Test.py
@@ -4,5 +4,5 @@ import Util.RoomUtil
 
 room = Util.RoomUtil.load_room("Example")
 up = UserParser(room)
-up.testing_method("look at the dog")
-#print(up.simplify_command("look at the dog"))
+#up.testing_method("go compass direction")
+print(up.simplify_command("look compass directon lock"))
