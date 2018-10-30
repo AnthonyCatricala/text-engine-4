2bed376aea0f9757fcf1161592104e7ae070ec47
Patrick Modica <modicap@sunyit.edu>
2018-10-30 14:49:48 -0400

    added item class, updated help menu

--- Help/HelpMenu.py
+++ Help/HelpMenu.py
@@ -1,4 +1,4 @@
-import sys
+
 
 def help_main():
     move = [
@@ -43,5 +43,27 @@ def help_main():
 def help_look():
     use = "look"
     desc = "\nUse the look command to look at the surroundings of the room you are in as well as its contents.\n"
+    print(use)
+    print(desc)
+
+def help_use():
+    use = "[use] [item]"
+    desc = "\nUse the use command to use an item that is in the inventory of the player. Most items will be " +
+    "consumable, meaning the item is removed from the players inventory on use but not all items will work " +
+    "like that. An example would be an item such as a key being used to unlock a door."
+    print(use)
+    print(desc)
+
+def help_get():
+    use = "[get] [item]"
+    desc = "\nUse the get command to put an item into your inventory. This command will not work for all items." +
+    " An example would be using the get command on a bookcase and expecting it to be in your inventory."
+    print(use)
+    print(desc)
+
+def help_examine():
+    use = "[examine] [item]"
+    desc = "\nuse the examine command to examine a specific item that is in the room the player is in. It will give the " +
+    "user a more detailed description of the item that is being targeted."
     print(use)
     print(desc)
