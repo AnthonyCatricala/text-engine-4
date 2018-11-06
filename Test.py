59e4c049ace4bc01268798411efc83b9fda43c77
oliveriolucy@yahoo.com <oliveriolucy@yahoo.com>
2018-11-06 14:49:11 -0500

    Reverting push.

--- Test.py
+++ Test.py
@@ -2,9 +2,9 @@ from Parser.UserParser import UserParser
 import Util.RoomUtil
 
 
-#room = Util.RoomUtil.load_room("Example")
+room = Util.RoomUtil.load_room("Example")
 #print(room)
-up = UserParser("Test Room")
+up = UserParser(room)
 #up.testing_method("go compass direction")
-#print(up.simplify_command("l compass direction"))
+print(up.simplify_command("l compass direction"))
 #Util.RoomUtil.save_room(room)
